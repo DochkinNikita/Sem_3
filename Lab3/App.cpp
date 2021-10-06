c0c84f478c508c56687f698d2f15232f0d181dd2
DochkinNikita <73274829+DochkinNikita@users.noreply.github.com>
2021-10-06 23:04:19 +0700

    Add files via upload

---
+++ Lab3/App.cpp
@@ -0,0 +1,51 @@
+#include "App.h"
+
+void App::start(){
+
+	List list;
+
+	list.getListFromFile(const_cast<char*>("Date.txt"));
+
+	int key = -1;
+	do {
+
+		cout << "----MENU----" << endl;
+		cout << "1) Show All Date's" << endl;
+		cout << "2) Show in format \"Date - Next Date\"" << endl;
+		cout << "3) Show system date and time" << endl;
+		cout << "0) Exit" << endl << endl;
+
+		cout << "Insert key->"; cin >> key;
+
+		switch (key)
+		{
+		case 1: {
+
+			list.listPrint();
+
+			break;
+		}
+		
+		case 2: {
+
+			list.listPrintSort();
+
+			break;
+		}
+
+		case 3: {
+			datetime sysTime;
+			sysTime.showSysDate();
+			break;
+		}
+
+		default:
+
+			if (key != 0) cout << "Error! Not found in menu!" << endl;
+
+			break;
+		}
+
+	} while (key != 0);
+
+}
